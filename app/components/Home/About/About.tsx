dcaf3fc9fe85321b768ae984af3019884de02548
Jagdish <jagdishjena93@gmail.com>
2025-09-03 16:22:53 +0530

    About Page Complete

---
+++ app/components/Home/About/About.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { BiMoney } from "react-icons/bi";
+import { BsCheck2Square } from "react-icons/bs";
+import AboutImage from "@/public/images/about.png";
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <div className="pt-20 pb-16 bg-[#001232]">
+      <div className="text-center p-2">
+        <h1 className="text-[22px] md:text-[30px] text-white font-semibold">
+          About Us
+        </h1>
+        <h1 className="text-[15px] mt-1 md:text-[17px] text-white opacity-70">
+          We use the latest technologies and tools in order to create a better
+          code
+        </h1>
+      </div>
+
+      {/* {define grid system} */}
+      <div className="w-4/5 mt-14 items-center md:mt-20 mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12">
+        {/* {Text Content } */}
+        <div>
+          <h1 className="text-[19px] md:text-[25px] font-semibold text-white">
+            A faster way to do just about anything
+          </h1>
+          <p className="text-white opacity-60 mt-4  ">
+            Clarinet accustomed. Would legs of framework officers. We've to
+            morning like a contracting him, the the to said in need gradually
+            wellfeigned.
+          </p>
+
+          {/* iconList item 1*/}
+          <div className="flex mt-8 items-center space-x-4  ">
+            {/* icon box */}
+            <div className="w-12 h-12 rounded-full bg-pink-700 flex flex-col items-center justify-center ">
+              <BsCheck2Square className="w-[1.3rem] h-[1.3rem] text-white" />
+            </div>
+            {/*icon text content */}
+            <div>
+              <h1 className="text-white text-[16px] md:text-[22px] font-semibold">
+                Licensed & Certified
+              </h1>
+              <p className="text-white mt-1 text-[15px] opacity-70 ">
+                Lorem ipsum dolor sit amet. Lorem, ipsum.
+              </p>
+            </div>
+          </div>
+          {/* iconList item 2*/}
+          <div className="flex mt-8 items-center space-x-4  ">
+            {/* icon box */}
+            <div className="w-12 h-12 rounded-full bg-blue-700 flex flex-col items-center justify-center ">
+              <BiMoney className="w-[1.3rem] h-[1.3rem] text-white" />
+            </div>
+            {/*icon text content */}
+            <div>
+              <h1 className="text-white text-[16px] md:text-[22px] font-semibold">
+                Savings & Investment
+              </h1>
+              <p className="text-white mt-1 text-[15px] opacity-70 ">
+                Lorem ipsum dolor sit amet. Lorem, ipsum.
+              </p>
+            </div>
+          </div>
+
+          {/* iconList item 2*/}
+          <div className="flex mt-8 items-center space-x-4  ">
+            {/* icon box */}
+            <div className="w-12 h-12 rounded-full bg-orange-500 flex flex-col items-center justify-center ">
+              <BiMoney className="w-[1.3rem] h-[1.3rem] text-white" />
+            </div>
+            {/*icon text content */}
+            <div>
+              <h1 className="text-white text-[16px] md:text-[22px] font-semibold">
+                100% Secure Investment
+              </h1>
+              <p className="text-white mt-1 text-[15px] opacity-70 ">
+                Lorem ipsum dolor sit amet. Lorem, ipsum.
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* Image Content */}
+        <div>
+          <Image src={AboutImage} alt="about" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- app/components/Home/Navigation/Nav.tsx
+++ app/components/Home/Navigation/Nav.tsx
@@ -45,7 +45,10 @@ const Nav = ({ openNav }: Props) => {
         {/* {Login Button and navbar menu} */}
         <div className="flex items-center space-x-4 ">
           {/* {Login button} */}
-          <button className="rounded px-10 py-2.5 overflow-hidden group bg-yellow-500 hover:bg-gradient-to-r hover:from-yellow-500 hover:to-yellow-400 relative text-white hover:ring-2 hover:ring-offset-2 hover:ring-yellow-400 transition-all ease-out duration-300">
+          <button
+            suppressHydrationWarning
+            className="rounded px-10 py-2.5 overflow-hidden group bg-yellow-500 hover:bg-gradient-to-r hover:from-yellow-500 hover:to-yellow-400 relative text-white hover:ring-2 hover:ring-offset-2 hover:ring-yellow-400 transition-all ease-out duration-300"
+          >
             <span className="absolute right-0 w-8 h-32 mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
             Login
           </button>
